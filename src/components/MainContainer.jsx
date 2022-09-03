3b58bca53b5295782c9e778976ce4e4868d7d103
nk mills <emilewinworth@gmail.com>
2022-09-03 12:02:15 +0100

    50% done with homecontainer

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,25 +1,13 @@
 import React from "react";
-import Delivery from "../img/delivery.png";
+import HomeContainer from "./HomeContainer";
 
 const MainContainer = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-      <div className="py-2 flex-1 flex flex-col items-start md:items-center justify-center">
-        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
-          <p className="text-base text-orange-500 font-semibold">
-            Bike Delivery
-          </p>
-          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
-            <img src={Delivery} className="w-full h-full object-contain" />
-          </div>
-        </div>
-        <p className="text-[2.5rem] font-bold">
-          The Fastest Delivery in <span>Your City</span>
-        </p>
-      </div>
-      <div className="py-2 bg-blue-400 flex-1"></div>
+    <div className="w-full h-auto flex flex-col items-center justify-center">
+    <HomeContainer />
     </div>
   );
 };
 
 export default MainContainer;
+ 
