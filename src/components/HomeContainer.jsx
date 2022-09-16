7c8be99ad6143a004b03a446aae326637e74e305
nk mills <emilewinworth@gmail.com>
2022-09-16 14:14:25 +0100

    HomeContainer Completed

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -26,13 +26,13 @@ const HomeContainer = () => {
       <div className="py-2 flex-1 flex items-center relative">
         <img src={HeroBg} className="ml-auto h-420 w-full lg:w-auto lg:h-650" alt="hero-bg" />
 
-        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4">
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-4 gap-4 flex-wrap">
           {heroData && heroData.map(n =>(
-            <div key={n.id} className="w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center">
-            <img src={n.imageSrc} className="w-40 -mt-20" alt="I1" />
-            <p className="text-xl font-semibold text-textColor mt-4">{n.name}</p>
+            <div key={n.id} className="lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg">
+            <img src={n.imageSrc} className="w-20 lg:w-40 -mt-10 lg:-mt-20" alt="I1" />
+            <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">{n.name}</p>
 
-            <p className="text-sm text-lightTextGray font-semibold my-1">
+            <p className="text-[12px] lg:text-sm text-lightTextGray font-semibold my-1 lg:my-3">
               {n.desc}
             </p>
 
