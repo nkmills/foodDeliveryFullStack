a92b00c7049df2fbd478a6516ccf6262a3edad4a
nkmills <njoyaemile25@gmail.com>
2024-05-08 18:50:44 -0400

    Edited home container

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -10,7 +10,7 @@ const HomeContainer = () => {
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
-            Bike Delivery
+            Bike and Car Delivery
           </p>
           <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
             <img src={Delivery} className="w-full h-full object-contain" />
